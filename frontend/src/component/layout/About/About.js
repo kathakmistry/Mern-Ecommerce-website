e20b81cb384f37a4bd92557f3c618ff421676cae
KATHAK MISTRY <65577706+kathakmistry@users.noreply.github.com>
2022-07-27 11:34:20 +0530

    Add files via upload

---
+++ frontend/src/component/layout/About/About.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./aboutSection.css";
+import { Button, Typography, Avatar } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+const About = () => {
+  const visitInstagram = () => {
+    window.location = "https://www.instagram.com/";
+  };
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              src="https://res.cloudinary.com/dddihex7h/image/upload/v1658898553/ecommerce%20proj/Passport_photo_bjw31g.jpg"
+              alt="Founder"
+            />
+            <Typography>Kathak Mistry</Typography>
+            <Button onClick={visitInstagram} color="primary">
+              Visit Instagram
+            </Button>
+            <span>
+              This is a sample wesbite made to learn mern concepts
+            </span>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+            {/* <a
+              href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
+              target="blank"
+            >
+              <YouTubeIcon className="youtubeSvgIcon" />
+            </a> */}
+
+            <a href="https://instagram.com/meabhisingh" target="blank">
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
